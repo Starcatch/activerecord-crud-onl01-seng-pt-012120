83880874a4845fd1edbfefc34c5b46a1483c0b88
Timur Mishiev <timurmishiev@gmail.com>
2020-04-08 00:19:46 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -12,7 +12,6 @@ def can_be_instantiated_and_then_saved
 end
 
 def can_be_created_with_a_hash_of_attributes
-  # Initialize movie and then and save it
   attributes = {
       title: "The Sting",
       release_date: 1973,
@@ -24,10 +23,9 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block
-  
   Movie.create do |m|
     m.title = "Home Alone"
-     m.release_date = 1990
+    m.release_date = 1990
   end
 end
 
@@ -44,26 +42,28 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find(1).title
+  Movie.all.order(id: :desc).last.title
 end
 
 def can_find_by_multiple_attributes
+  # title == "Title"
+  # release_date == 2000
+  # director == "Me"
   Movie.find_by(title: "Title", release_date: 2000, director: "Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-   Movie.where("release_date > 2002").order(release_date: :desc)
+  Movie.where("release_date > 2002").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
-  # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
+  # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
   Movie.create(title: "Awesome Flick")
   movie = Movie.find_by(title: "Awesome Flick")
   movie.title = "Even Awesomer Flick"
   movie.save
-
 end
 
 def can_update_using_update_method
@@ -83,14 +83,15 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
+  movie=Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
   movie.destroy
-  
 end
 
 def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-   Movie.destroy_all
+  Movie.destroy_all
 end
+
+
